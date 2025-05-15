a5708c1f5a2be1549d23bc05c13e608412aaa34c
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-05-15 15:30:00 +0200

    creadas clases de prueba para paquetres dto, model, repository y service

---
+++ src/main/java/gestor_habitos/gestor_de_habitos_personal/dto/dtoPrueba.java
@@ -0,0 +1,4 @@
+package gestor_habitos.gestor_de_habitos_personal.dto;
+
+public class dtoPrueba {
+}
---
+++ src/main/java/gestor_habitos/gestor_de_habitos_personal/repository/repositoryPrueba.java
@@ -0,0 +1,4 @@
+package gestor_habitos.gestor_de_habitos_personal.repository;
+
+public class repositoryPrueba {
+}
