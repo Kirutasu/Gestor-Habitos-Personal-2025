e79592f3aa93e4e5fd18ebada1f2e388ae55ea1e
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-05-15 15:26:23 +0200

    Creada e implementada configuracion del proyecto: dependencias, estructura de paquetes, clase HelloController de prueba. Funciona

---
+++ src/main/java/gestor_habitos/gestor_de_habitos_personal/controller/HelloController.java
@@ -0,0 +1,13 @@
+package gestor_habitos.gestor_de_habitos_personal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping ("/")
+    public String home() {
+        return "Hola! La app funciona";
+    }
+}
