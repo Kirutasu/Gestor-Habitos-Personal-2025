8bb67d053a6ee00c48fbdc86691ea4c1372410ed
neosbamen <lorzproducciones@gmail.com>
2025-05-14 23:43:59 -1200

    1 creacion repo local

---
+++ src/main/java/gestor_habitos/gestor_de_habitos_personal/GestorDeHabitosPersonalApplication.java
@@ -0,0 +1,13 @@
+package gestor_habitos.gestor_de_habitos_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeHabitosPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeHabitosPersonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gestor_habitos/gestor_de_habitos_personal/GestorDeHabitosPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package gestor_habitos.gestor_de_habitos_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeHabitosPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
