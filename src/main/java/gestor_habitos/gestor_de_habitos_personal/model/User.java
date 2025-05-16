96a04b5fb2710d8f928e04c537a6c9c98234a9e3
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-05-16 19:49:07 +0200

    creadas variables, constructores, getters y setters para las clases Habit y User; Creadas clases ENUM; Borrada clase modelHabitPrueba

--- src/main/java/gestor_habitos/gestor_de_habitos_personal/model/User.java
+++ src/main/java/gestor_habitos/gestor_de_habitos_personal/model/User.java
@@ -24,4 +24,63 @@ public class User {
     @OneToMany (mappedBy = "usuario")
     private List<Habit> listaHabitosUsuario; // el conjunto de habitos de este usuario
 
+    public User() {
+    }
+
+    public User(int id, String nameUser, String email, String password, LocalDate fechaRegistro, List<Habit> listaHabitosUsuario) {
+        this.id = id;
+        this.nameUser = nameUser;
+        this.email = email;
+        this.password = password;
+        this.fechaRegistro = fechaRegistro;
+        this.listaHabitosUsuario = listaHabitosUsuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public List<Habit> getListaHabitosUsuario() {
+        return listaHabitosUsuario;
+    }
+
+    public void setListaHabitosUsuario(List<Habit> listaHabitosUsuario) {
+        this.listaHabitosUsuario = listaHabitosUsuario;
+    }
 }
